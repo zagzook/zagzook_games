7c35e3243d50c9adf1d371628a58c0c580f578cd
JefferyAdamsZagzook <zagzook@gmail.com>
2026-02-07 14:52:51 -0500

    Stop tracking config folder

--- config/custom_routing.php
+++
@@ -1,6 +0,0 @@
-<?php
-$routes = [
-    'tg-admin' => 'trongate_administrators/login',
-    'tg-admin/submit_login' => 'trongate_administrators/submit_login'
-];
-define('CUSTOM_ROUTES', $routes);
--- config/site_owner.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Settings to do with the identification of the site owner
-define('WEBSITE_NAME', 'Zagzook Games');
-define('OUR_NAME', 'Zagzook Games');
-define('OUR_TELNUM', '');
-define('OUR_ADDRESS', '');
-define('OUR_EMAIL_ADDRESS', '');
